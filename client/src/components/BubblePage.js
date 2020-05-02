3ab316672a025154f4a42b347424ed32d8735d61
Sterling Ford <mellowcf@gmail.com>
2020-05-02 13:40:06 -0400

    mvp and some stretch

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-
-import Bubbles from "./Bubbles";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import BubblesV2 from "./BubblesV2";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
@@ -9,10 +8,25 @@ const BubblePage = () => {
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+  useEffect(() => {
+
+    axiosWithAuth()
+      .get("http://localhost:5000/api/colors")
+      .then (response => {
+        console.log("Bubble data retrieved:", response);
+
+        setColorList(response.data);
+      })
+      .catch (error => {
+        console.log("Couldn't retrieve bubble data:", error);
+      })
+
+  }, [])
+
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
+      <BubblesV2 colors={colorList} />
     </>
   );
 };
